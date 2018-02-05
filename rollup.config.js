da90a5deb558d2357a3f09997bfd0dcc09f14fb8
Darren Poon <dyhpoon@gmail.com>
2018-02-06 01:19:34 +0800

    fix output dir

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,22 @@
-import typescript from 'rollup-plugin-typescript'
+import rollupTs from 'rollup-plugin-typescript'
+import ts from 'typescript'
+import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    format: 'cjs',
-    file: 'index.js',
-    dir: 'dist',
-  },
-  plugins: [typescript()],
+  output: [
+    {
+      format: 'cjs',
+      file: pkg.main,
+    },
+    {
+      format: 'es',
+      file: pkg.module,
+    },
+  ],
+  plugins: [
+    rollupTs({
+      typescript: ts,
+    }),
+  ],
 }
